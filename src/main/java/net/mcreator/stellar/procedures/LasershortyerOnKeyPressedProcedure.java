e27a2b6cfecdd1c06ebd3b8472e5e1c2d9931de5
POV-I-GOATED <134237541+adderblock@users.noreply.github.com>
2024-05-03 17:28:50 -0500

    david

--- src/main/java/net/mcreator/stellar/procedures/LasershortyerOnKeyPressedProcedure.java
+++ src/main/java/net/mcreator/stellar/procedures/LasershortyerOnKeyPressedProcedure.java
@@ -1,20 +1,31 @@
 package net.mcreator.stellar.procedures;
 
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
 
 import net.mcreator.stellar.network.StellarModVariables;
 import net.mcreator.stellar.init.StellarModEntities;
 import net.mcreator.stellar.entity.LaserEntity;
+import net.mcreator.stellar.StellarMod;
 
 public class LasershortyerOnKeyPressedProcedure {
-	public static void execute(Entity entity) {
+	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
 		if ((entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new StellarModVariables.PlayerVariables())).Role == 1) {
-			if ((entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new StellarModVariables.PlayerVariables())).lasercoold == 1) {
+			if ((entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new StellarModVariables.PlayerVariables())).lasercoold == 0) {
+				{
+					double _setval = 1;
+					entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+						capability.lasercoold = _setval;
+						capability.syncPlayerVariables(entity);
+					});
+				}
 				{
 					Entity _shootFrom = entity;
 					Level projectileLevel = _shootFrom.level();
@@ -34,6 +45,18 @@ public class LasershortyerOnKeyPressedProcedure {
 						projectileLevel.addFreshEntity(_entityToSpawn);
 					}
 				}
+				StellarMod.queueServerWork(10, () -> {
+					{
+						double _setval = 0;
+						entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.lasercoold = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+				});
+			} else {
+				if (entity instanceof Player _player && !_player.level().isClientSide())
+					_player.displayClientMessage(Component.literal("Cooldown"), true);
 			}
 		}
 	}
