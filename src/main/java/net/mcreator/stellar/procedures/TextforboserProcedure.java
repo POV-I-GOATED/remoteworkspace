6dc0df5caa7ac4ccfb1fee93142176f6eb2a88b1
POV-I-GOATED <134237541+adderblock@users.noreply.github.com>
2024-05-03 16:43:55 -0500

    87

--- src/main/java/net/mcreator/stellar/procedures/TextforboserProcedure.java
+++ src/main/java/net/mcreator/stellar/procedures/TextforboserProcedure.java
@@ -9,7 +9,7 @@ public class TextforboserProcedure {
 		if (entity == null)
 			return "";
 		String Texts = "";
-		return "Strong" + new java.text.DecimalFormat("##.##").format((entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new StellarModVariables.PlayerVariables())).StrongLuck) + " Luck"
+		return "Strong" + new java.text.DecimalFormat("##.##").format((entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new StellarModVariables.PlayerVariables())).StrongLuck) + " Luck "
 				+ new java.text.DecimalFormat("##.##").format((entity.getCapability(StellarModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new StellarModVariables.PlayerVariables())).ChanceLuck - 2);
 	}
 }
